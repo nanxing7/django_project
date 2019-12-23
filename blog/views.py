de9d6700688df417bfd17684164f4df88c519cc8
sanyuedev <sanyuedev@gmail.com>
2019-12-23 14:19:56 +0800

    增加密码模块

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,22 @@
-from django.shortcuts import render,get_object_or_404
-from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib.auth.models import User
 from django.views.generic import (ListView,
-    DetailView,
-    CreateView,
-    UpdateView,
-    DeleteView)
+                                  DetailView,
+                                  CreateView,
+                                  UpdateView,
+                                  DeleteView)
 from .models import Post
+
 
 def home(request):
     context = {'posts': Post.objects.all()}
     return render(request, 'blog/home.html', context)
 
+
 class PostListView(ListView):
     model = Post
-    template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html
+    template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted']
     paginate_by = 5
@@ -22,36 +24,37 @@ class PostListView(ListView):
 
 class UserPostListView(ListView):
     model = Post
-    template_name = 'blog/user_posts.html' # <app>/<model>_<viewtype>.html
+    template_name = 'blog/user_posts.html'  # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted']
     paginate_by = 5
 
     def get_queryset(self):
-        user = get_object_or_404(User,username=self.kwargs.get('username'))
-        return Post.objects.filter(author=user).ordery_by('-date_posted')
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 
 class PostDetailView(DetailView):
     model = Post
 
 
-class PostCreateView(LoginRequiredMixin,CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
-    fields = ['title','content']
+    fields = ['title', 'content']
 
-    def form_valid(self,form):
+    def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
-class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView,):
-    model = Post
-    fields = ['title','content']
 
-    def form_valid(self,form):
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView, ):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
-    
+
     def test_func(self):
         post = self.get_object()
         if self.request.user == post.author:
@@ -59,7 +62,7 @@ class PostUpdateView(LoginRequiredMixin,UserPassesTestMixin,UpdateView,):
         return False
 
 
-class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     success_url = '/'
 
@@ -72,4 +75,3 @@ class PostDeleteView(LoginRequiredMixin,UserPassesTestMixin,DeleteView):
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
-
--- django_project/settings.py
+++ django_project/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR,'meida')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'meida')
 MEDIA_URL = '/media/'
 
 # CRISPY_FORMS 模版版本
@@ -126,3 +126,12 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'blog-home'
 # 登录URL
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+
+
