456bd3678b0938107597fcc6bf836b00f88f7093
sanyuedev <sanyuedev@gmail.com>
2019-12-24 15:05:32 +0800

    修改media

--- django_project/settings.py
+++ django_project/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'meida')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 # CRISPY_FORMS 模版版本
